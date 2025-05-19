6026f7c08501c5f4ab23ea17f64e4282270e41be
Shreevaishnavibaddala <95173786+Shreevaishnavibaddala@users.noreply.github.com>
2025-05-19 21:19:35 +0530

    Moved NLTK to req.txt

--- plagiarism_app.py
+++ plagiarism_app.py
@@ -9,9 +9,6 @@ from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 from nltk.stem import WordNetLemmatizer
 
-nltk.download('punkt')
-nltk.download('stopwords')
-nltk.download('wordnet')
 
 stop_words = set(stopwords.words('english'))
 lemmatizer = WordNetLemmatizer()
